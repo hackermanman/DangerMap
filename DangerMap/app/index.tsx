73167cb8777197032f949ddd56406fea79a292ca
Nicholas Columbus <nick.g.columbus@gmail.com>
2025-03-26 12:30:18 -0400

    added in an about and not found screen

---
+++ DangerMap/app/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "expo-router"; 
+import { Text, View, StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>DANGERRA MAP DEMO</Text>
+      <Link href={"/about"} style={styles.button}>ABOUT</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:"#1e1e1e",
+  },
+  text:{
+    color:"white",
+  },
+  button:{
+    fontSize:20,
+    textDecorationLine:"underline",
+    color:"red",
+  },
+});
+
