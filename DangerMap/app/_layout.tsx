fbb50b160f5dec9b7ab085a92bbe5722270c5e8c
hackermanman <nick.g.columbus@gmail.com>
2025-05-20 12:27:45 -0400

    Firebase stuff trying to fix errors

--- DangerMap/app/_layout.tsx
+++ DangerMap/app/_layout.tsx
@@ -1,19 +1,54 @@
-import { Stack } from 'expo-router';
-import { LogBox } from "react-native"; 
-
-LogBox.ignoreAllLogs(true);
+import { Stack, useRouter, useSegments } from 'expo-router';
+import { useEffect, useState } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" 
-        options={{
-          headerShown: false,
-        }} />
-        <Stack.Screen name="not-found" 
-        options={{ 
+	const [initializing, setInitializing] = useState(true);
+	const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+	const router = useRouter();
+	const segments = useSegments();
 
-        }} />
-    </Stack>
-  );
+	const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+		console.log('onAuthStateChanged', user);
+		setUser(user);
+		if (initializing) setInitializing(false);
+	};
+
+	useEffect(() => {
+		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+		return subscriber;
+	}, []);
+
+	useEffect(() => {
+		if (initializing) return;
+
+		const inAuthGroup = segments[0] === '(tabs)';
+
+		if (user && !inAuthGroup) {
+			router.replace('/(tabs)/index');
+		} else if (!user && inAuthGroup) {
+			router.replace('/');
+		}
+	}, [user, initializing]);
+
+	if (initializing)
+		return (
+			<View
+				style={{
+					alignItems: 'center',
+					justifyContent: 'center',
+					flex: 1
+				}}
+			>
+				<ActivityIndicator size="large" />
+			</View>
+		);
+
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ title: 'Login' }} />
+			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+		</Stack>
+	);
 }
